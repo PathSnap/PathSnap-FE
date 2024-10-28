34ab0afa172f9f79d26c8dd39e05d030a6a6bc9a
heedong12 <dlgmldus3289@naver.com>
2024-10-28 22:05:41 +0900

    design: add shadow

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,14 +16,19 @@ const Footer: React.FC = () => {
   const [selectedFooterIndex, setSelectedFooterIndex] = useState<number>(0);
 
   return (
-    <div className="max-w-[500px] w-full fixed h-20 bottom-0 flex justify-around items-center">
+    <div className="max-w-[500px] w-full fixed h-20 bottom-0 flex justify-around items-center shadow-xs rounded-t-2xl ">
+      <div
+        className={
+          'absolute bg-primary w-[68px] h-[68px] rounded-full bottom-[42px]'
+        }
+      ></div>
+
       {menuItems.map((menuItem, menuIndex) => {
         const Component = menuItem.component;
-
         return (
           <div
             key={menuIndex}
-            className="flex flex-col justify-center items-center gap-1.5"
+            className="flex flex-col justify-center items-center gap-1.5 "
             onClick={() => setSelectedFooterIndex(menuIndex)}
           >
             <Component isActive={menuIndex === selectedFooterIndex} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#77CEBD',
+      },
+      boxShadow: {
+        xs: '0px -8px 12px rgba(0, 0, 0, 0.08)',
+      },
+    },
   },
   plugins: [],
 };
