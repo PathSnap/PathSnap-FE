21ba4a3262208fa1c9b18a923806752fc94ac2ca
heedong12 <dlgmldus3289@naver.com>
2024-11-03 03:13:27 +0900

    design: publishing bottom sheet when activate

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,14 @@ export default {
       colors: {
         primary: '#77CEBD',
         second: '#595959',
+        third: '#999999',
       },
       textColor: (theme) => ({ ...theme('colors') }),
       boxShadow: {
         xxs: '0px 0px 8px rgba(0, 0, 0, 0.1)',
         xs: '0px -8px 12px rgba(0, 0, 0, 0.08)',
-        m: '0 -4px 8px rgba(0, 0, 0, 0.1)',
+        m: '0px -4px 8px rgba(0, 0, 0, 0.1)',
+        l: '0px 0px 6px rgba(0, 0, 0, 0.15)',
       },
     },
   },
