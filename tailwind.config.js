609d5ed9192c9312ce7aff533979460175b801ab
woojin065 <marty0605@naver.com>
2024-11-03 11:34:13 +0900

    테일윈드로 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,22 @@ export default {
     extend: {
       colors: {
         primary: '#77CEBD',
+        primaryLight: '#a0e5e5',
+        primaryDark: '#44b8b8',
+        borderLight: '#e6f9f9',
       },
       boxShadow: {
         xs: '0px -8px 12px rgba(0, 0, 0, 0.08)',
+      },
+      keyframes: {
+        glow: {
+          '0%': { boxShadow: '0 0 5px 5px rgba(160, 229, 229, 0.5)' },
+          '50%': { boxShadow: '0 0 15px 15px rgba(0, 0, 0, 0.3)' },
+          '100%': { boxShadow: '0 0 5px 5px rgba(160, 229, 229, 0.5)' },
+        },
+      },
+      animation: {
+        glow: 'glow 2s infinite',
       },
     },
   },
