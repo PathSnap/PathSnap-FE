474957178f049c65d829850b1c7f504ece622128
heedong12 <dlgmldus3289@naver.com>
2024-11-11 14:33:30 +0900

    design: 바텀시트와 푸터 없는 레이아웃 추가

--- src/layouts/BottomAndFooterLayout.tsx
+++ src/layouts/BottomAndFooterLayout.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import BottomSheet from '../components/BottomSheet/BottomSheet';
 import Footer from '../components/Footer';
+import { Outlet } from 'react-router';
 
-interface BottomAndFooterLayoutProps {
-  children: React.ReactNode;
-}
-
-const BottomAndFooterLayout: React.FC<BottomAndFooterLayoutProps> = ({
-  children,
-}) => {
+const BottomAndFooterLayout: React.FC = ({}) => {
   return (
     // 푸터 높이 빼줌
     <div className={'flex flex-col w-full h-[calc(100%-80px)] relative'}>
       {/* BASE */}
-      <div className={'flex-grow'}>{children}</div>
+      <div className={'flex-grow'}>
+        <Outlet />
+      </div>
       <BottomSheet />
       <Footer />
     </div>
---
+++ src/pages/AddFriend.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddFriend = () => {
+  return <div>친추</div>;
+};
+
+export default AddFriend;
