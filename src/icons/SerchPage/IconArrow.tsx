ba15ce9b6b4b708ae68aee171aafe8a6851b4154
woojin065 <123820750+woojin065@users.noreply.github.com>
2024-11-25 02:43:24 +0900

    design: 아이콘 생성

--- src/icons/SerchPage/IconArrow.tsx
+++ src/icons/SerchPage/IconArrow.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 
-interface IconArrowLeftProps {
+interface IconArrowProps {
   width?: number;
   height?: number;
   color?: string;
   className?: string;
+  direction?: 'left' | 'right'; // 방향 설정
 }
 
-const IconArrowLeft: React.FC<IconArrowLeftProps> = ({
+const IconArrow: React.FC<IconArrowProps> = ({
   width = 12,
   height = 22,
   color = 'black',
   className,
+  direction = 'left', // 기본 방향은 left
 }) => {
+  // 오른쪽 방향일 경우 회전 스타일 추가
+  const rotationStyle = direction === 'right' ? 'transform rotate-180' : '';
+
   return (
     <svg
       width={width}
@@ -20,7 +25,7 @@ const IconArrowLeft: React.FC<IconArrowLeftProps> = ({
       viewBox="0 0 12 22"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className={className}
+      className={`${className} ${rotationStyle}`}
     >
       <path
         d="M11 1L1 11L11 21"
@@ -32,4 +37,4 @@ const IconArrowLeft: React.FC<IconArrowLeftProps> = ({
   );
 };
 
-export default IconArrowLeft;
+export default IconArrow;
